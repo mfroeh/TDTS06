78d4f5e70734a5d758ee7c9f797dd0feef7b5f87
mfroeh <mfroeh0@protonmail.com>
2022-10-11 23:02:11 +0200

    Refine lab4

--- distace-vector-routing/RouterNode.py
+++ distace-vector-routing/RouterNode.py
@@ -1,118 +1,115 @@
 #!/usr/bin/env python
 from typing import List
 from RouterSimulator import RouterSimulator
-import GuiTextArea
+from GuiTextArea import GuiTextArea
 from RouterPacket import RouterPacket
 from copy import deepcopy
 
-INFTY: int = 0
-N: int = 0
+INFTY: int
+N: int
 
 
 class RouterNode:
-    ID: int = -1
-    myGUI = None
-    sim = None
+    ID: int
+    myGUI: GuiTextArea
+    sim: RouterSimulator
 
-    costs: List[int] = list()
-    routesList: List[int] = list()
-    D: List[List[int]] = list()
+    costs: List[int]
+    neighbors: List[int]
+    routes: List[int]
+    distances: List[List[int]]
 
     def __init__(self, ID: int, sim: RouterSimulator, costs: List[int]):
         self.ID = ID
         self.sim = sim
-        self.myGUI = GuiTextArea.GuiTextArea(
-            "  Output window for Router #" + str(ID) + "  "
-        )
+        self.myGUI = GuiTextArea("  Output window for Router #" + str(ID) + "  ")
 
         # Setup constants
         global N, INFTY
         N = sim.NUM_NODES
         INFTY = sim.INFINITY
 
+        # Setup costs
         self.costs = deepcopy(costs)
-        self.routesList = [i if costs[i] != INFTY else -1 for i in range(N)]
-        # for i in range(N):
-        #     if self.costs[i] != INFTY:
-        #         self.routesList[i] = i
-        #     else:
-        #         self.routesList[i] = -1
 
-        self.D = [[INFTY for j in range(N)] for i in range(N)]
-        self.D[ID] = deepcopy(costs)
+        # Setup neighbors
+        self.neighbors = [y for y in range(N) if costs[y] != INFTY and y != ID]
 
-        self.sendUpdate()
-        self.printDistanceTable()
+        # Setup routes for neighbors
+        self.routes = [y if y in self.neighbors else -1 for y in range(N)]
+        self.routes[ID] = ID
 
-    def update(self) -> bool:
+        # Setup distance matrix
+        self.distances = [[INFTY for m in range(N)] for n in range(N)]
+        self.distances[ID] = deepcopy(costs)
+
+        # Send first update
+        self.sendUpdates()
+
+    def update(self):
         changed = False
-        for i in range(N):
-            if i == self.ID:
+        for y in range(N):
+            if y == self.ID:
                 continue
 
-            newRouteCost = INFTY
+            min_cost = INFTY
+            min_route = -1
+            # Test all neighbors for better routes
+            for v in self.neighbors:
+                # Cost is going from neighbor to y and from us to neighbor
+                cost = self.costs[v] + self.distances[v][y]
+                if cost < min_cost:
+                    min_cost = cost
+                    min_route = v
 
-            oldRoute = self.routesList[i]
-            oldRouteCost = self.D[self.ID][i]
-
-            for j in range(N):
-                if j == self.ID:
-                    continue
-
-                if (
-                    self.costs[j] != INFTY
-                    and newRouteCost > self.D[j][i] + self.costs[j]
-                    and j != self.ID
-                ):
-                    self.D[self.ID][i] = self.D[j][i] + self.costs[j]
-                    newRouteCost = self.D[self.ID][i]
-                    self.routesList[i] = j
-
-            if oldRoute != self.routesList[i] or oldRouteCost != self.D[self.ID][i]:
+            # If we found a new minimal cost or the route changed
+            if self.distances[self.ID][y] != min_cost or self.routes[y] != min_route:
+                self.distances[self.ID][y] = min_cost
+                self.routes[y] = min_route
                 changed = True
 
-        return changed
+        if changed:
+            self.sendUpdates()
 
     def recvUpdate(self, pkt):
         self.myGUI.println(
             f"[Router {self.ID}] Received distance vector from Router {pkt.sourceid}"
         )
-        self.D[pkt.sourceid] = pkt.mincost
-        if self.update():
-            self.sendUpdate()
-
-    def sendUpdate(self):
-        for i in range(N):
-            sendedCosts = deepcopy(self.D[self.ID])
-
-            if i == self.ID or self.costs[i] == INFTY:
-                continue
-
-            if self.sim.POISONREVERSE:
-                for j in range(N):
-                    if self.routesList[j] == i and j != i:
-                        sendedCosts[j] = INFTY
-
-            pkt = RouterPacket(self.ID, i, sendedCosts)
-            self.sim.toLayer2(pkt)
+        # Set new distance vector and see if we can update
+        self.distances[pkt.sourceid] = pkt.mincost
+        self.update()
 
     def updateLinkCost(self, dest, newcost):
         self.myGUI.println(
             f"[Router {self.ID}] Received link cost update to destination {dest}: old({self.costs[dest]}), new({newcost})"
         )
 
+        # Set new link cost and update
         self.costs[dest] = newcost
-        if self.update():
-            self.sendUpdate()
+        self.update()
 
     def printDistanceTable(self):
         self.myGUI.println(f"Routing table for {self.ID} at {self.sim.getClocktime()}")
-        # self.myGUI.println("--------------------")
-        # for n in range(N):
-        #     self.myGUI.print(f"{n}:  ")
-        #     for m in range(N):
-        #         self.myGUI.print(f"{self.D[n][m]} | ")
-        #     self.myGUI.println("")
-        self.myGUI.println(f"D_{self.ID}: {self.D[self.ID]}")
-        self.myGUI.println(f"R_{self.ID}: {self.routesList}")
-        self.myGUI.println("---")
+        self.myGUI.println("-----------------")
+        for n in range(N):
+            self.myGUI.print(f"{n}:  ")
+            for m in range(N):
+                self.myGUI.print(f"{self.distances[n][m]} | ")
+            self.myGUI.println("")
+        self.myGUI.println(f"Routes: {self.ID}: {self.routes}")
+        self.myGUI.println("-----------------")
+
+    def sendUpdates(self):
+        for v in self.neighbors:
+            distance_vector = deepcopy(self.distances[self.ID])
+
+            if self.sim.POISONREVERSE:
+                for y in range(N):
+                    # Set to infty if we are going through v for y, but v is not the goal node
+                    if self.routes[y] == v and y != v:
+                        distance_vector[y] = INFTY
+
+            self.myGUI.println(
+                f"[Router {self.ID}] Sent my distance vector {distance_vector} to Router {v}"
+            )
+            self.sim.toLayer2(RouterPacket(self.ID, v, distance_vector))
